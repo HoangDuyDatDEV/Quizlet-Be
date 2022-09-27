74469a073366d03f08f132017e144624c5e028bc
ducanh501 <lbducanh140401@gmail.com>
2022-09-27 17:24:06 +0700

    uploap-api-to-view

--- Quizlet_Be/Quizlet_API/views.py
+++ Quizlet_Be/Quizlet_API/views.py
@@ -1,4 +1,5 @@
 from sre_constants import SUCCESS
+from unittest import result
 
 from xmlrpc.client import ResponseError
 from .models import *
@@ -152,11 +153,16 @@ def delete_class(request, pk):
 @api_view(['GET'])
 def search_class(request):
     keyword = request.GET.get('keyword','')
-    Class_list = Class.objects.filter(
-        Q(classname__icontains = keyword)
-    )
+    Class_list = Class.objects.all()
+    if keyword:   
+      Class_list = Class.objects.filter(
+          Q(classname__icontains = keyword)
+      )
+    
+    total = Class_list.count()
     data = ClassSerializer(Class_list, many = True).data
-    return Response(data)
+    result = {'total':total, 'data':data}
+    return Response(result)
         
 
 @api_view(['GET'])
